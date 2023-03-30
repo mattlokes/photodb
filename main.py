cec37f7c91d1e4f7870607847ce3520597ec9e7e
mattlokes <matlokes@gmail.com>
2023-03-30 11:26:36 +0000

    Remove thumb nail hacks to allow for google drive migration, now expects a different database structure

--- main.py
+++ main.py
@@ -13,19 +13,18 @@ if 'PHOTODB_DB_NAME' not in os.environ:
 import tinyurl
 
 db_connect = create_engine('sqlite:///data/{}'.format(os.environ['PHOTODB_DB_NAME']))
+conn = db_connect.connect() # connect to database
 app = Flask(__name__, static_folder='static')
 api = Api(app)
 
 #RESTFUL API
 class PhotoDB(Resource):
     def get(self, collection_name, primary):
-        conn = db_connect.connect() # connect to database
         query = conn.execute("select * from {0} where photo_primary = {1}".format(collection_name, primary) )
         #print(query.cursor.fetchall())
         return {'photos': [ (i[1],i[2],i[3]) for i in query.cursor.fetchall()]}
     
     def post(self,collection_name, primary):
-        conn = db_connect.connect()
         
         if collection_name == "create":   #CREATE TABLE
             print("Creating New Table Entry....{0}".format(primary))
@@ -50,22 +49,32 @@ class PhotoDB(Resource):
 
 api.add_resource(PhotoDB, '/rest/<collection_name>/<primary>')
 
+def queryDictList( queryStr ):
+    query = conn.execute(queryStr)
+    return [ dict(zip(query.keys(), i)) for i in query.cursor.fetchall() ]
+
 #GALLERY PAGE
 @app.route('/gallery/<collection>')
 @app.route('/gallery/<collection>/')
 @app.route('/gallery/<collection>/<pic>')
 def photodb_gallery(collection, pic=None):
-    conn = db_connect.connect() # connect to database
 
-    query = conn.execute("select * from {0} where photo_primary = 1".format(collection) )
-    pImage = [ (i[1],i[2],i[3]) for i in query.cursor.fetchall() if "thumb" not in i[1] ]
-    
-    query = conn.execute("select * from {0} where photo_primary = 0".format(collection) )
-    sImage = [ (i[1],i[2],i[3]) for i in query.cursor.fetchall()]
+    tImages = { t['grp'] : t for t in queryDictList(f"select * from {collection} where thumb = 1") }
+    pImages = { p['grp'] : p for p in queryDictList(f"select * from {collection} where photo_primary = 1") }
+    # Multiple Secondary Images need appending
+    sImages = {}
+    for s in  queryDictList(f"select * from {collection} where photo_primary = 0 and thumb = 0"):
+        sImages[s['grp']] = sImages.get(s['grp'],[]) + [s]
+
+    images = {}
+    for t in tImages.keys():
+        images[t] = { 'thumb':None, 'primary':None, 'secondary':[] }
+        images[t]['thumb']     = tImages.get(t, None)
+        images[t]['primary']   = pImages.get(t, None)
+        images[t]['secondary'] = sImages.get(t, None)
 
     event_name = collection.replace("_", " ").title() 
-    return render_template('index.html', event_name=event_name, grid_image=pImage, 
-                                         hidden_image=sImage, pic=pic)
+    return render_template('index.html', event_name=event_name, images=images, pic=pic)
 
 @app.route('/gallery/fonts/<filename>')  
 def send_font(filename):  
